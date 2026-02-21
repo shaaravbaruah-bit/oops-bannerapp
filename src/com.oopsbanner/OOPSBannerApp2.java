c412678fa4c36c85bd818576719ea4b95b00d2be
shaaravbaruah-bit <shaaravbaruah@gmail.com>
2026-02-22 01:56:40 +0530

    Feature (Uc2):Print OOPS to console

--- src/com.oopsbanner/OOPSBannerApp2.java
+++ src/com.oopsbanner/OOPSBannerApp2.java
@@ -1,11 +1,13 @@
-public class OOPSBannerApp2{
-public static void main(String[] args){
-		System.out.println("  *****   " + "  *****   " + "  ******  " + "  ******  ");
-        System.out.println(" **   **  " + " **   **  " + " **    ** " + " **       ");
-        System.out.println(" **   **  " + " **   **  " + " **    ** " + " **       ");
-        System.out.println(" **   **  " + " **   **  " + " ******   " + "  *****   ");
-        System.out.println(" **   **  " + " **   **  " + " **       " + "       ** ");
-        System.out.println(" **   **  " + " **   **  " + " **       " + " **    ** ");
-        System.out.println("  *****   " + "  *****   " + " **       " + "  ******  ");
-	}
+public class OOPSBannerApp2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    ******    ***** ");
+        System.out.println("**   **  **   **  **   **  **      ");
+        System.out.println("**   **  **   **  **   **  *****   ");
+        System.out.println("**   **  **   **  ******       **  ");
+        System.out.println("**   **  **   **  **           **  ");
+        System.out.println("**   **  **   **  **      **   **  ");
+        System.out.println(" *****    *****   **       *****   ");
+    }
 }
